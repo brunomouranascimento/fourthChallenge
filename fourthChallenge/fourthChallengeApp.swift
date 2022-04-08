1b5dc2ea15d72589406af2b491387f6c255d3f69
Bruno Nascimento <bruno.bmn@gmail.com>
2022-04-08 16:54:15 -0300

    Initial Commit

---
+++ fourthChallenge/fourthChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fourthChallengeApp.swift
+//  fourthChallenge
+//
+//  Created by Bruno Nascimento on 08/04/22.
+//
+
+import SwiftUI
+
+@main
+struct fourthChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
